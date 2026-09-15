1c04b067cb0985d50eaafcc49280d70f9320a544
nabilChemkhi <nabil.chemkhi@esprit.tn>
2026-09-15 07:33:25 +0200

    Add clear items functionality

--- app.js
+++ app.js
@@ -1,72 +1,83 @@
-"use strict";
-
 // ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.querySelector('.grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-const alert = document.querySelector(".alert");
-const form = document.querySelector(".grocery-form");
-const grocery = document.querySelector(".grocery");
-const submitBtn = document.querySelector(".submit-btn");
-const container = document.querySelector(".grocery-container");
-const list = document.querySelector(".grocery-list");
-const clearBtn = document.querySelector(".clear-btn");
-
-// Edit option
+// edit option
 let editElement;
 let editFlag = false;
 let editID = "";
 
 // ****** EVENT LISTENERS **********
-
-form.addEventListener("submit", addItem);
+// submit form
+form.addEventListener('submit', addItem);
+// clear items
+clearBtn.addEventListener('click', clearItems);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
 
-function addItem(event) {
-  event.preventDefault();
-
-  const value = grocery.value.trim();
+  const value = grocery.value;
   const id = new Date().getTime().toString();
 
   if (value && !editFlag) {
-    const element = document.createElement("article");
+    const element = document.createElement('article');
 
-    // Add class
-    element.classList.add("grocery-item");
+    // add class
+    element.classList.add('grocery-item');
 
-    // Add ID
-    element.setAttribute("data-id", id);
+    // add id
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
 
-    element.innerHTML = `
-      <p class="title">${value}</p>
+    element.innerHTML = `<p class="title">${value}</p>
 
-      <div class="btn-container">
-        <button type="button" class="edit-btn">
-          <i class="fas fa-edit"></i>
-        </button>
+                    <div class="btn-container">
+                        <!-- Edit button -->
+                        <button type="button" class="edit-btn">
+                            <i class="fas fa-edit"></i>
+                        </button>
 
-        <button type="button" class="delete-btn">
-          <i class="fas fa-trash"></i>
-        </button>
-      </div>
-    `;
+                        <!-- Delete button -->
+                        <button type="button" class="delete-btn">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </div>`;
 
-    // Add item to the list
+    // add to the list
     list.appendChild(element);
 
-    // Display alert
-    displayAlert("Item added to the list", "success");
+    // display alert
+    displayAlert('Item added to the list', 'success');
 
-    // Show container
-    container.classList.add("show-container");
+    // show container
+    container.classList.add('show-container');
 
-    // Clear input
-    grocery.value = "";
+    //add to local storage
+    addToLocalStorage(id, value);
+
+    //set back to default
+    setBackToDefault();
+
+    // clear input
+    grocery.value = '';
+  } else if (value && editFlag) {
+    editElement.querySelector('.title').textContent = value;
+    displayAlert('Value changed', 'success');
+    editLocalStorage(editID, value);
+    setBackToDefault();
   } else {
-    displayAlert("Please enter a value", "danger");
+    displayAlert('Please enter a value', 'danger');
   }
 }
 
-// Display alert
+// display alert
 function displayAlert(text, action) {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
@@ -75,3 +86,31 @@ function displayAlert(text, action) {
     alert.classList.remove(`alert-${action}`);
   }, 3000);
 }
+// clear items
+function clearItems() {
+    const items = document.querySelectorAll('.grocery-item');
+    if (items.length > 0) {
+        items.forEach(item => {
+            list.removeChild(item);
+        });
+        displayAlert('List cleared', 'success');
+        container.classList.remove('show-container');
+        setBackToDefault();
+        // localStorage.removeItem('list');
+    }
+}
+
+//set back to default
+function setBackToDefault() {
+    grocery.value = '';
+    editFlag = false;
+    editID = '';
+    submitBtn.textContent = 'submit';
+}
+
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+    console.log('added to local storage');
+}
+
+// ****** SETUP ITEMS **********
